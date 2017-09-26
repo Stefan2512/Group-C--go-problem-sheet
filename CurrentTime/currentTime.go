b8219f102526159d4a0b5528b2fa8f7936a5e9e2
Stefan Radu <Core2quad###!>
2017-09-26 21:11:23 +0100

    Current Time
    Write a program that prints the current time and date to the console.

---
+++ CurrentTime/currentTime.go
@@ -0,0 +1,15 @@
+//Scope: Write a program that prints the current time and date to the console.
+//Student Name: Stefan Radu
+
+package main
+
+import (
+"fmt"
+"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
